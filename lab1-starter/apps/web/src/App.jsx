e6e75c1a93e0dafc97b33330853c6e6406aa1788
Moussa Marena <marenamoussa444@gmail.com>
2026-06-02 09:45:01 +0000

    installe l'application React avec Vite dans apps/web

---
+++ lab1-starter/apps/web/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>SénEvent - Bienvenue sur React</h1>
+      <p>Prochaine sous-étape : Migrer nos événements ici !</p>
+    </div>
+  )
+}
+
+export default App
